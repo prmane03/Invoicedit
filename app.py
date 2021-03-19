f9a1f71977cd6de9950c70810aa6ea2f224288a1
Pratik Mane <49906534+prmane03@users.noreply.github.com>
2021-03-19 11:25:15 +0530

    Update app.py
    Changed database url

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import os
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get(DATABASE_URL)
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL')
 
 db=SQLAlchemy(app)
 
